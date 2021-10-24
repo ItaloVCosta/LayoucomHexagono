4066d47d51679f15585ba5ee8f09b468ba01c00f
Italo Costa <italo.costa99@gmail.com>
2021-10-24 17:17:31 -0300

    Criando server API funcionando pt2

--- Server.js
+++ Server.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app= express()
 const bodyParser= require('body-parser')
 const fetch = require('cross-fetch')
-
+const { response } = require('express')
 app.use(bodyParser.urlencoded({extended: true}))
-app.post('/RetornoServer',(req,resp) =>{
+
+
+const showData = (resultado)=>{
+            console.log(resultado.localidade)
+         
+}
+
+app.post('/RetornoServer',(req,resp) => {
     console.log(req.body)
     let Renda =req.body.Renda
     const Percapita = Renda/req.body.Dependentes
@@ -12,8 +19,12 @@ app.post('/RetornoServer',(req,resp) =>{
     CEP = CEP.replace(/[^\d]+/g,'')
     const URL =`https://viacep.com.br/ws/${CEP}/json`
     console.log(URL)
-    fetch(URL).then(response =>response.json()).then(console.log)
-    
+
+
+    fetch(URL)
+        .then(response =>{response.json()
+            .then(data => showData(data))
+        })
     if(req.body.fullname != '')
         resp.send("Nome: " + req.body.fullname + "<br> Endereço: " + req.body.CEP + "<br> Renda per capita: " + Percapita.toFixed(2) )
     else
